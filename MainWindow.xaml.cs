60d24b7f72517127da17b7fd999916a03bc33685
KarolBrz <61661483+Karol1302@users.noreply.github.com>
2022-05-12 20:19:28 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DelegacjeProjekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Database1Entities _db = new Database1Entities();
+        public static DataGrid datagrid;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load();
+        }
+
+        private void Load()
+        {
+            myDataGrid.ItemsSource = _db.Delegacja.ToList();
+            datagrid = myDataGrid;
+        }
+
+        private void InsertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Wstawianie wstawianie = new Wstawianie();
+            wstawianie.ShowDialog();
+        }
+    }
+}
---
+++ WstawianieDel.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DelegacjeProjekt
+{
+    /// <summary>
+    /// Interaction logic for Wstawianie.xaml
+    /// </summary>
+    public partial class Wstawianie : Window
+    {
+        Database1Entities _db = new Database1Entities();
+        public Wstawianie()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadd_Click(object sender, RoutedEventArgs e)
+        {
+            Delegacja delegacja = new Delegacja();
+            {
+                IdDelegacji = IDBox.Text;
+                Termin = TerminBox.Text;
+                Miejscowosc = MiejscBox.Text;
+            };
+            _db.Delegacja.Add(nowadelegacja);
+            _db.SaveChanges();
+            MainWindow.datagrid.ItemsSource = _db.Delegacja.ToList();
+            this.Hide();
+
+        }
+    }
+}
